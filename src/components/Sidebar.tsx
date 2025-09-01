42401b6aec8e272c14b8c796a25b7216a52cac8d
Kittipong <anonymous51124@gmail.com>
2025-09-01 14:27:22 +0700

    edit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -55,7 +55,7 @@ export default function Sidebar() {
             <Avatar
               size="45px"
               radius="xl"
-              src="../public/Profile.png"
+              src="./public/Profile.png"
             />
           </Indicator>
           <Text>User : Kittipong : Admin</Text>
